1896d01047e40874ad7b6471a9b2e6e7d1d901f7
mjcumming <mike@4831.com>
2025-05-24 12:15:09 +0000

    test

---
+++ tests_backup/const.py
@@ -0,0 +1,9 @@
+"""Constants for WiiM tests."""
+from custom_components.wiim.const import (
+    CONF_PASSWORD,
+)
+from custom_components.wiim.const import (
+    CONF_USERNAME,
+)
+
+MOCK_CONFIG = {CONF_USERNAME: "test_username", CONF_PASSWORD: "test_password"}
